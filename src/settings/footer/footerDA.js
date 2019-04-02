dcb9bcd35d9ebe9eb631eab589415cd5cf14b5d0
Vikasini <vikasininithi@gmail.com>
2019-04-02 13:11:47 +0530

    create

---
+++ src/settings/footer/footerDA.js
@@ -0,0 +1,21 @@
+var Footer = require('../../model/footer.model');
+var appSetting = require('../../config/appSetting');
+
+
+
+exports.getFooterDetails = function (req, res) {
+    Footer.find({}).select().exec(function (err, footerData) {
+        if (err) {
+            res.status(500).send({
+                "result": 'error occured while retreiving data'
+            })
+        } else {
+            if(footerData.length !== 0) {
+                footerData[0].logoImageName = appSetting.logoServerPath + footerData[0].logoImageName;
+                res.status(200).json(footerData);
+            }
+         
+        }
+    })
+}
+
